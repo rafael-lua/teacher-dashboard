c0c0a906cee6682d70f2c1507b5afbcb69e4dd48
Rafael Oliveira <rafael.tragueta79@gmail.com>
2021-06-04 18:05:30 -0300

    refactor dashboard view logic into smaller components

---
+++ src/components/dashboard/DashboardTopNavigation.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-app-bar color="green darken-1 white--text fixIndex">
+    <v-container
+      fill-height
+      fluid
+      class="d-flex justify-space-between pa-0 ma-0s"
+    >
+      <p class="text-uppercase font-weight-medium text-body-2 pa-0 ma-0">
+        log-out
+      </p>
+      <p class="font-weight-regular pa-0 ma-0">
+        Welcome, <span class="font-weight-bold">{teacher name}</span>
+      </p>
+    </v-container>
+  </v-app-bar>
+</template>
+
+<script>
+/**
+ * The top navigation bar of the main dashboard, with user information and general status
+ */
+export default {
+  name: "DashboardTopNavigation",
+};
+</script>
+
+<style scoped>
+.fixIndex {
+  z-index: 1;
+}
+</style>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,55 +1,15 @@
 <template>
   <v-container fluid class="pa-0">
     <!-- dashboard main navigation  -->
-    <v-app-bar color="green darken-1 white--text fixIndex">
-      <v-container
-        fill-height
-        fluid
-        class="d-flex justify-space-between pa-0 ma-0s"
-      >
-        <p class="text-uppercase font-weight-medium text-body-2 pa-0 ma-0">
-          log-out
-        </p>
-        <p class="font-weight-regular pa-0 ma-0">
-          Welcome, <span class="font-weight-bold">{teacher name}</span>
-        </p>
-      </v-container>
-    </v-app-bar>
+    <DashboardTopNavigation />
 
     <v-row>
       <v-col>
         <!-- dashboard left control panel -->
-        <v-navigation-drawer permanent cliped class="lighten-5">
-          <v-list shaped dense>
-            <v-list-item class="py-1">
-              <v-list-item-content>
-                <v-list-item-title class="text-h6 text-uppercase">
-                  dashboard
-                </v-list-item-title>
-                <v-list-item-subtitle>General controllers</v-list-item-subtitle>
-              </v-list-item-content>
-            </v-list-item>
-
-            <v-divider></v-divider>
-
-            <v-list-item-group
-              mandatory
-              v-model="currentControllerId"
-              color="cyan darken-2"
-            >
-              <v-list-item rounded @click="setController('overview')">
-                <v-list-item-content> Overview </v-list-item-content>
-              </v-list-item>
-              <v-list-item @click="setController('subjects')">
-                <v-list-item-content> Subjects </v-list-item-content>
-              </v-list-item>
-              <v-list-item @click="setController('students')">
-                <v-list-item-content> Students </v-list-item-content>
-              </v-list-item>
-            </v-list-item-group>
-          </v-list>
-        </v-navigation-drawer>
+        <DashboardControlPanel @setController="setController" />
       </v-col>
+
+      <!-- main content area for the controller components ocupy -->
       <v-col>
         <v-container>
           <div v-if="view('overview')">Overview</div>
@@ -62,15 +22,22 @@
 </template>
 
 <script>
+import DashboardTopNavigation from "../components/dashboard/DashboardTopNavigation";
+import DashboardControlPanel from "../components/dashboard/DashboardControlPanel";
+
 /**
  * Teacher dashboard for interaction with the system
  */
 export default {
   name: "Dashboard",
 
+  components: {
+    DashboardTopNavigation,
+    DashboardControlPanel,
+  },
+
   data() {
     return {
-      currentControllerId: 0,
       controller: "overview",
     };
   },
@@ -86,9 +53,3 @@ export default {
   },
 };
 </script>
-
-<style scoped>
-.fixIndex {
-  z-index: 1;
-}
-</style>
