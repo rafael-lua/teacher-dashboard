f50dc4e4e2921ca50625991f12733445d7f9183f
Rafael Oliveira <rafael.tragueta79@gmail.com>
2021-06-04 12:34:48 -0300

    add main nav bar setup

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,6 +1,13 @@
 <template>
   <v-container fluid>
-    <p class="text-h3">Hello!</p>
+    <v-app-bar color="blue-grey darken-3 white--text" app>
+      <v-container fill-height fluid class="d-flex justify-space-between">
+        <p class="text-uppercase font-weight-medium text-body-2">log-out</p>
+        <p class="font-weight-regular">
+          Welcome, <span class="font-weight-bold">{teacher name}</span>
+        </p>
+      </v-container>
+    </v-app-bar>
   </v-container>
 </template>
 
