80fc0a600c20a5e409c4e33502ce07dece8ec6db
Rafael Oliveira <rafael.tragueta79@gmail.com>
2021-06-05 16:30:39 -0300

    update form reponsiveness

--- src/components/authentication/LoginForm.vue
+++ src/components/authentication/LoginForm.vue
@@ -1,10 +1,10 @@
 <template>
   <v-form v-model="isValid">
     <v-container class="d-flex justify-center">
-      <v-card elevation="1" class="pa-2" outlined shaped width="50%">
+      <v-card elevation="1" class="pa-2" outlined shaped :width="cardWidth">
         <v-card-title>Log-in in to start:</v-card-title>
         <v-row class="d-flex justify-center">
-          <v-col cols="6">
+          <v-col cols="10">
             <v-text-field
               v-model="username"
               append-icon="mdi-account"
@@ -20,12 +20,32 @@
               label="Password"
               @click:append="showPassword = !showPassword"
             ></v-text-field>
-            <div class="d-flex justify-space-between">
-              <v-btn color="success" elevation="2" rounded>
+            <div
+              class="
+                d-flex
+                flex-column
+                align-center
+                flex-sm-row
+                justify-space-between
+              "
+            >
+              <v-btn
+                color="success"
+                elevation="2"
+                rounded
+                class="my-2 my-sm-0"
+                width="12em"
+              >
                 Log-In
                 <v-icon right dark> mdi-account-arrow-right </v-icon>
               </v-btn>
-              <v-btn color="primary" elevation="2" rounded>
+              <v-btn
+                color="primary"
+                elevation="2"
+                rounded
+                class="my-2 my-sm-0"
+                width="10em"
+              >
                 Sign Up
                 <v-icon right dark> mdi-pencil-plus </v-icon>
               </v-btn>
@@ -48,6 +68,23 @@ export default {
       password: "",
       showPassword: false,
     };
+  },
+
+  computed: {
+    cardWidth() {
+      switch (this.$vuetify.breakpoint.name) {
+        case "xs":
+          return "95%";
+        case "sm":
+          return "75%";
+        case "md":
+        case "lg":
+        case "xl":
+          return "50%";
+        default:
+          return "100%";
+      }
+    },
   },
 };
 </script>
