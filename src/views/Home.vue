511dff36a81cbfff3554cbb055c2d499acab25e7
Rafael Oliveira <rafael.tragueta79@gmail.com>
2021-06-07 11:58:01 -0300

    update home dashboard button with icon

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,9 +6,10 @@
         <v-divider class="my-7"></v-divider>
         <v-container v-if="authenticated" fluid class="d-flex justify-center">
           <div class="d-flex flex-column">
-            <v-btn x-large color="success" @click="gotoDashboard" class="my-5"
-              >DASHBOARD</v-btn
-            >
+            <v-btn x-large color="success" @click="gotoDashboard" class="my-5">
+              DASHBOARD
+              <v-icon medium class="ml-2">mdi-view-dashboard</v-icon>
+            </v-btn>
             <p class="text-subtitle-1">You are already logged in :)</p>
           </div>
         </v-container>
